c21e3a2fbfdcaf5a2bc56b94725d1b8c6490e32b
komagic <910367438@qq.com>
2017-08-30 23:02:08 +0800

    0.0.1

--- src/client/index.js
+++ src/client/index.js
@@ -2,11 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import rootReducer from 'reducers';
-import { createStore } from 'redux';
 import routes from '../common/routes';
 import './index.less';
+import thunk from 'redux-thunk';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 
-const store = createStore(rootReducer)
+const store = createStore(combineReducers(rootReducer),applyMiddleware(thunk));
+
 
 ReactDOM.render(
   <Provider store={store}>
